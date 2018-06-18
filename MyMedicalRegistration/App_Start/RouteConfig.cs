3c57ff66db0037d4737a18c0670d1d38bc81e030
nikesh-maharjan <nikesh.cap@gmail.com>
2018-06-18 13:16:10 -0400

    18 June 2018 second commit

--- MyMedicalRegistration/App_Start/RouteConfig.cs
+++ MyMedicalRegistration/App_Start/RouteConfig.cs
@@ -20,6 +20,12 @@ namespace MyMedicalRegistration
             );
 
             routes.MapRoute(
+                name: "Index",
+                url: "index",
+                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                );
+
+            routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
                 defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
